2bdec692cefbf548007c396c995123ad01878871
FrankTimmons <franktimmonspdx@gmail.com>
2022-05-31 10:26:28 -0700

    Make all starter files, add setup to index.html, and add functions to scripts.js
    Co-authored-by: Zac Waggoner <github-zacwaggoner42@gmail.com>

---
+++ index.html
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <title>Calculator</title>
+</head>
+<body>
+  <h1>Calculator</h1>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+const number1 = parseInt(prompt("Enter a number: "));
+const number2 = parseInt(prompt("Enter another number: "));
+
+alert(add(number1, number2));
