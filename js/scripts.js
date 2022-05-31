60064438fb7992959286ad53db42b3f0419083b2
FrankTimmons <franktimmonspdx@gmail.com>
2022-05-31 10:38:29 -0700

    Add add, multiply, divide, and celsiusToFarenheit functions.
    Co-authored-by: Zac Waggoner <github-zacwaggoner42@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,20 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-const number1 = parseInt(prompt("Enter a number: "));
-const number2 = parseInt(prompt("Enter another number: "));
+function multiply(number1, number2) {
+  return number1 * number2;
+}
 
-alert(add(number1, number2));
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function celsiusToFarenheit(number1) {
+  return add(divide(multiply(number1, 9),5),32)
+}
+
+const number1 = parseInt(prompt("Enter a degree in celsius: "));
+//const number2 = parseInt(prompt("Enter another number: "));
+const result = celsiusToFarenheit(number1);
+
+alert(result);
